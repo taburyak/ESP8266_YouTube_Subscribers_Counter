68dbf9d0d38b2406d7ac2e12a36eae3c368a2a7e
Andriy Honcharenko <taburyak@gmail.com>
2022-06-04 05:33:11 +0300

    add image

--- include/StateHandler.h
+++ include/StateHandler.h
@@ -12,6 +12,7 @@
 #include <Adafruit_ILI9341.h>
 #include <SimpleTimer.h>
 #include <SecretDefinitions.h>
+#include <images.h>
 
 // #include <YoutubeApi.h>
 // #include <WiFiClientSecure.h>
@@ -48,9 +49,10 @@ void timerRefreshData();
 
 void enterInitialPeriph()
 {
-    tft.begin(25000000U);
+    tft.begin();
     tft.setRotation(2);
     tft.fillScreen(ILI9341_BLACK);
+    tft.drawRGBBitmap(45, 160, hotWether, 150, 150);
 
     ServiceState::set(MODE_CONNECTING_NET);
 
@@ -176,54 +178,3 @@ void timerRefreshData()
 {
     ServiceState::set(MODE_REFRESH_DATA);
 }
-
-//---------------------------------------------------------
-  // time_t epochTime = timeClient.getEpochTime();
-  // Serial.print("Epoch Time: ");
-  // Serial.println(epochTime);
-  
-  // String formattedTime = timeClient.getFormattedTime();
-  // Serial.print("Formatted Time: ");
-  // Serial.println(formattedTime);  
-
-  // int currentHour = timeClient.getHours();
-  // Serial.print("Hour: ");
-  // Serial.println(currentHour);  
-
-  // int currentMinute = timeClient.getMinutes();
-  // Serial.print("Minutes: ");
-  // Serial.println(currentMinute); 
-   
-  // int currentSecond = timeClient.getSeconds();
-  // Serial.print("Seconds: ");
-  // Serial.println(currentSecond);  
-
-  // String weekDay = weekDays[timeClient.getDay()];
-  // Serial.print("Week Day: ");
-  // Serial.println(weekDay);    
-
-  // //Get a time structure
-  // struct tm *ptm = gmtime ((time_t *)&epochTime); 
-
-  // int monthDay = ptm->tm_mday;
-  // Serial.print("Month day: ");
-  // Serial.println(monthDay);
-
-  // int currentMonth = ptm->tm_mon+1;
-  // Serial.print("Month: ");
-  // Serial.println(currentMonth);
-
-  // String currentMonthName = months[currentMonth-1];
-  // Serial.print("Month name: ");
-  // Serial.println(currentMonthName);
-
-  // int currentYear = ptm->tm_year+1900;
-  // Serial.print("Year: ");
-  // Serial.println(currentYear);
-
-  // //Print complete date:
-  // String currentDate = String(currentYear) + "-" + String(currentMonth) + "-" + String(monthDay);
-  // Serial.print("Current date: ");
-  // Serial.println(currentDate);
-
-  // Serial.println("");
